98d509a824a73808817662d9d4a2c3f661796331
copidiaz17 <copidiaz17@gmail.com>
2026-01-26 12:16:17 -0300

    reversa de cambios

--- routes/obras.js
+++ routes/obras.js
@@ -310,33 +310,7 @@ router.post(
 
 
    
-  /* ================================================
-   ELIMINAR OBRA (DELETE) - SOLO ADMIN
-   ================================================ */
-router.delete(
-  "/:id",
-  authMiddleware,
-  hasRole([ROLES.ADMIN]),
-  async (req, res) => {
-    try {
-      const obra = await Obra.findByPk(req.params.id);
-
-      if (!obra) {
-        return res.status(404).json({ message: "Obra no encontrada" });
-      }
-
-      await obra.destroy();
-
-      res.json({ message: "Obra eliminada correctamente" });
-    } catch (error) {
-      console.error("Error al eliminar obra:", error);
-      res.status(500).json({
-        message: "Error interno al eliminar la obra",
-      });
-    }
-  }
-);
-
+  
 
 
   export default router;
